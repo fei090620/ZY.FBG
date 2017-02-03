c852b3510bb2448a1f1839299e7174630bd53c8b
zhouyang <yzhou3_05@sina.com>
2017-02-03 15:26:52 +0800

    添加足球Agent

--- ZY.FBG/Test/ZY.FBG.Engine.DemoTest/Program.cs
+++ ZY.FBG/Test/ZY.FBG.Engine.DemoTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,13 +13,13 @@ namespace ZY.FBG.Engine.DemoTest
     {
         static void Main(string[] args)
         {
-            GameEngine g = GameEngine.Instance;
-            g.SetGameTime(1);
-            g.Start();
+            //GameEngine g = GameEngine.Instance;
+            //g.SetGameTime(1);
+            //g.Start();f
             //Thread.Sleep(2000);
             //g.SetGameTime(2);
             //g.Start();
-
+            Console.WriteLine(GCSettings.IsServerGC);
             Console.ReadKey();
         }
     }
---
+++ ZY.FBG/ZY.FBG.Engine/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZY.FBG.Engine
+{
+    /// <summary>
+    /// 命令接口
+    /// </summary>
+    public interface ICommand : IComparable
+    {
+
+    }
+}
